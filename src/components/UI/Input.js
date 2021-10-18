21e920265cfb2f6d7ec72b9f3a3b7de271d1bd81
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-18 12:11:21 -0400

    Create Input component

---
+++ src/components/UI/Input.js
@@ -0,0 +1,10 @@
+const Input = props => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  );
+};
+
+export default Input;
