de833918c81a62cebb3b3ba86a18db845a3cc431
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-18 12:48:57 -0400

    Portaled Modal above root in DOM and add Modal to Cart component

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,13 +1,27 @@
+import classes from './Modal.module.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const Backdrop = props => {
   return <div className={classes.backdrop} />;
 };
 
-const Modal = props => {
+const ModalOverlay = props => {
   return (
     <div className={classes.modal}>
       <div className={classes.conent}>{props.children}</div>
     </div>
   );
+};
+
+const Modal = () => {
+  <React.Fragment>
+    {ReactDOM.createPortal(<Backdrop />, document.getElementById('overlays'))}
+    {ReactDOM.createPort(
+      <ModalOverlay>{props.children}</ModalOverlay>,
+      document.getElementById('overlays')
+    )}
+  </React.Fragment>;
 };
 
 export default Modal;
