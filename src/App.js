84cd98e642ba2bd6f1c6d7b0a925f010e62ac128
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-22 12:28:03 -0400

    Pass down onClick function to show cart modal from App to HeaderCartButton

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <React.Fragment>
       {cartIsShown && <Cart />}
-      <Header />
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
