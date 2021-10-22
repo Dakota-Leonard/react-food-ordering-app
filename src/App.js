70d38cca02d5a51711f3b6c51b19eaedf6a59ab3
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-22 12:24:23 -0400

    Add cart and modal handling functions to manage state in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Header from './components/Layout/Header';
-import React from 'react';
+import React, { useState } from 'react';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => setCartIsShown(true);
+
+  const hideCardHandler = () => setCartIsShown(false);
+
   return (
     <React.Fragment>
-      <Cart />
+      {cartIsShown && <Cart />}
       <Header />
       <main>
         <Meals />
