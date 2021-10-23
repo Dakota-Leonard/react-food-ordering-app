d8a6e2919583494d3a5fddcdc3552519526f0865
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-22 20:58:47 -0400

    Create reducer and action types for adding items to cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,37 @@
 import CartContext from './cart-context';
+import { useReducer } from 'react';
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      const updatedItems = [...state.items, action.item];
+      const updatedTotalAmount =
+        state.totalAmount + action.item.price * action.item.amount;
+      return { items: updatedItems, total: updatedTotalAmount };
+    default:
+      return defaultCartState;
+  }
+};
 
 const CartProvider = props => {
-  const addItemToCartHandler = item => {};
+  const [cartState, dispatch] = useReducer(cartReducer, defaultCartState);
 
-  const removeItemFromCardHandler = id => {};
+  const addItemToCartHandler = item => {
+    dispatch({ type: 'ADD', item });
+  };
+
+  const removeItemFromCardHandler = id => {
+    dispatch({ type: 'REMOVE', id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCardHandler,
   };
