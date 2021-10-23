8910a1a0845fcfb62fb23e27a8e451a825fc7faf
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-22 21:06:55 -0400

    Use ref in meal item to track state instead of two way binding/controlling component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,28 @@
+import { useRef } from 'react';
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
 
 const MealItemForm = props => {
+  const amountInputRef = useRef();
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const enteredAmount = +amountInputRef.current.value;
+
+    if (
+      !enteredAmount.trim().length ||
+      enteredAmount < 1 ||
+      enteredAmount > 5
+    ) {
+      return;
+    }
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: 'amount_' + props.id,
