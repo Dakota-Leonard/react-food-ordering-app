1649888bd572a8f821f5937d140618c751f3dbcb
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-24 16:44:54 -0400

    Add useEffect to handle animation state and class change on checkout button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,41 @@
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../store/cart-context';
-import { useContext } from 'react';
 
 const HeaderCartButton = props => {
   const cartCtx = useContext(CartContext);
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
+
+  //Animate cart icon when cart is updated
+  const btnClasses = `${classes.button} ${
+    buttonIsHighlighted ? classes.bump : ''
+  }`;
+
+  const { items } = cartCtx;
+
+  useEffect(() => {
+    if (items.length > 0) {
+      setButtonIsHighlighted(true);
+    } else {
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   const numberOfCartItems = cartCtx.items.reduce((acc, item) => {
     return acc + item.amount;
   }, 0);
 
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
